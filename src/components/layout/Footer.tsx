0777a046d472f11f1c0c95aeb095daa975edbab8
Bảo Phước <ayaseyuuki2003@gmail.com>
2025-12-04 17:45:41 +0700

    feat: Link new pages (Products, Resources, Careers) in Header and Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,20 +8,20 @@ const Footer: React.FC = () => {
       <div className={styles.content}>
         <div className={styles.column}>
           <h4>Product</h4>
-          <Link href="/">Features</Link>
+          <Link href="/products">Features</Link>
           <Link href="/solutions">Solutions</Link>
-          <Link href="/">Pricing</Link>
+          <Link href="/products">Pricing</Link>
         </div>
         <div className={styles.column}>
           <h4>Resources</h4>
-          <Link href="/">Documentation</Link>
-          <Link href="/">API Reference</Link>
-          <Link href="/">Blog</Link>
+          <Link href="/resources">Documentation</Link>
+          <Link href="/resources">API Reference</Link>
+          <Link href="/resources">Blog</Link>
         </div>
         <div className={styles.column}>
           <h4>Company</h4>
           <Link href="/about">About Us</Link>
-          <Link href="/">Careers</Link>
+          <Link href="/careers">Careers</Link>
           <Link href="/contact">Contact</Link>
         </div>
         <div className={styles.column}>
